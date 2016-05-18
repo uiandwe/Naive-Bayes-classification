8fe88747b7da15d29ebc8d3be9457ea00a036088
hyeonsj <hyeonsj@cimple.kr>
2016-05-18 20:29:49 +0900

    db 에러 처리 추가

--- db_connect.py
+++ db_connect.py
@@ -9,23 +9,37 @@ class Connect:
     cur = None
 
     def __init__(self):
-        conn = pymysql.connect(host=init.host, user=init.user, passwd=init.passwd, db=init.db, charset=init.charset)
-        self.conn = conn
-        self.cur = conn.cursor()
+        try:
+            conn = pymysql.connect(host=init.host, user=init.user, passwd=init.passwd, db=init.db, charset=init.charset)
+            self.conn = conn
+            self.cur = conn.cursor()
+        except pymysql.InternalError as error:
+            code, message = error.args
+            print(">>>>>>>>>>>>>", code, message)
 
     def get_cursor(self):
         return self.cur
 
     def find(self, sql):
         return_cur = None
-        self.cur.execute(sql)
-        for response in self.cur:
-            return_cur = response[0]
+
+        try:
+            self.cur.execute(sql)
+            for response in self.cur:
+                return_cur = response[0]
+        except pymysql.InternalError as error:
+            code, message = error.args
+            print(">>>>>>>>>>>>>", code, message)
+
         return return_cur
 
     def close_db(self):
-        self.cur.close()
-        self.conn.close()
+        try:
+            self.cur.close()
+            self.conn.close()
+        except pymysql.InternalError as error:
+            code, message = error.args
+            print(">>>>>>>>>>>>>", code, message)
 
 
 
