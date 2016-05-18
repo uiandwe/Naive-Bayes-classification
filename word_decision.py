b6fb8df0244c866fb3fce3c3ba445c2293f2b5d3
hyeonsj <hyeonsj@cimple.kr>
2016-05-18 22:18:35 +0900

    입력 / 전체 단어 카운트 함수 도출

--- word_decision.py
+++ word_decision.py
@@ -1,9 +1,7 @@
 from konlpy.tag import Kkma
 import db_connect
-
 db = db_connect.Connect()
 
-kkma = Kkma()
 
 #조건부 확률 계산
 def conditional_probability(test, train_table, all_count, table_count):
@@ -20,8 +18,6 @@ def conditional_probability(test, train_table, all_count, table_count):
         list_count.append(counter)
         counter = 0
 
-    print(list_count)
-
     cal_list = []
     for j in range(len(list_count)):
         #다음에 * 연산할떄 0이 되지 않기 위해 +1을 해줌
@@ -31,43 +27,53 @@ def conditional_probability(test, train_table, all_count, table_count):
         result *= float(round(cal_list[j], 6))
 
     return_value = float(result)*float(1.0/2.0)
-    print(return_value)
+    print(train_table, list_count, " value = ", return_value)
     return return_value
 
-# get the data
-input_file = open('input.txt', 'r')
 
-list_positive = []
-list_negative = []
+#입력 문장
+def get_input_file():
+    kkma = Kkma()
+    # get the data
+    input_file = open('input.txt', 'r')
 
-#형태소 분리
-input_line = input_file.readline()
-input_kkma = kkma.pos(input_line)
+    #형태소 분리
+    input_line = input_file.readline()
+    input_kkma_str = kkma.pos(input_line)
 
-print(input_kkma)
-
-test_output = []
-for i in input_kkma:
-    test_output.append(i[0])
-
-#긍정 단어 전체 갯수
-sql = "SELECT count(*) as cnt  FROM positive "
-cur = db.find(sql)
-positive_count = int(cur)
-
-#부정 단어 전체 갯수
-sql = "SELECT count(*) as cnt  FROM negative "
-cur = db.find(sql)
-negative_count = int(cur)
-
-# naive bayes 값 계산
-result_pos = conditional_probability(test_output, 'positive', positive_count+negative_count, positive_count)
-result_neg = conditional_probability(test_output, 'negative', positive_count+negative_count, negative_count)
-
-if result_pos > result_neg:
-    print(u'긍정')
-else:
-    print(u'부정')
+    return input_kkma_str
 
 
-db.close_db()
+#긍정 부정 단어 전체 갯수
+def get_word_counts():
+
+    sql = "SELECT count(*) as cnt  FROM positive "
+    cur = db.find(sql)
+    positive_word_count = int(cur)
+
+    #부정 단어 전체 갯수
+    sql = "SELECT count(*) as cnt  FROM negative "
+    cur = db.find(sql)
+    negative_word_count = int(cur)
+
+    return positive_word_count, negative_word_count
+
+if __name__ == '__main__':
+    input_kkma = get_input_file()
+    print(input_kkma)
+    test_output = []
+    for i in input_kkma:
+        test_output.append(i[0])
+
+    positive_count, negative_count = get_word_counts()
+
+    # naive bayes 값 계산
+    result_pos = conditional_probability(test_output, 'positive', positive_count+negative_count, positive_count)
+    result_neg = conditional_probability(test_output, 'negative', positive_count+negative_count, negative_count)
+
+    if result_pos > result_neg:
+        print(u'긍정')
+    else:
+        print(u'부정')
+
+    db.close_db()
